c8f8d23ebd761dfe6fa9cbefad55c540a8ce7405
Hiba Abdel Karim <abdelkarim.hiba@outlook.com>
2024-11-30 16:06:41 +0200

    Added a new body background

--- src/components/SearchTable.jsx
+++ src/components/SearchTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from "react";
 import { useNavigate } from "react-router-dom";
 import Skeleton from "@mui/material/Skeleton";
+
 export default function SearchTableSection({
   users,
   isloading,
